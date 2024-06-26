1d58fc6ddcb35e044926c5a3c16f6d9e11a4b28a
legobt <6wbvkn0j@anonaddy.me>
2024-06-26 06:25:54 +0000

    feat: caddy+acng for caching npm packages
    - chore: update caddy test

--- sidecars/apt-cacher-ng/entrypoint.sh
+++ sidecars/apt-cacher-ng/entrypoint.sh
@@ -11,6 +11,12 @@ elif [[ "${1}" == apt-cacher-ng || "${1}" == $(command -v apt-cacher-ng) ]]; the
   set --
 fi
 
+#mkdir -p /var/{cache,log.run}/apt-cacher-ng
+#chown -R apt-cacher-ng:apt-cacher-ng /var/{cache,log.run}/apt-cacher-ng
+#ls -la /var/*/apt-cacher-ng
+##echo
+#grep apt /etc/passwd
+
 # default behaviour is to launch apt-cacher-ng
 if [[ -z "${1}" ]]; then
   exec start-stop-daemon --start \
