e8ddc77ae982de68bfc07beae0c03d6085cc4b3a
legobt <6wbvkn0j@anonaddy.me>
2024-06-27 13:07:35 +0000

    test: add neovim typescript lsp integration test

---
+++ test/lsp-js/ht-test-1-1.sh
@@ -0,0 +1,35 @@
+#/!bin/bash
+# senss keystrokes to neovim (navigate to element and inspect) and screenscrapes for expected output
+result="$(
+  (
+    sleep 10
+    echo '{ "type": "sendKeys", "keys": ["w"] }'
+    sleep 0.2
+    echo '{ "type": "sendKeys", "keys": ["w"] }'
+    sleep 0.2
+    echo '{ "type": "sendKeys", "keys": [" "] }'
+    sleep 0.2
+    echo '{ "type": "sendKeys", "keys": ["\""] }'
+    sleep 1
+    echo '{ "type": "getView" }'
+    echo '{ "type": "sendKeys", "keys": [":LspInfo", "Enter"] }'
+    sleep 0.5
+    echo '{ "type": "getView" }'
+    echo '{ "type": "sendKeys", "keys": [":q", "Enter"] }'
+  ) \
+    | ht nvim test/lsp-js/fixtures/1/index.ts
+)"
+
+echo "$result" \
+  | grep --quiet 'class Struct<Type = unknown, Schema = unknown>.*encapsulate the validation logic.*typescript'
+if (( $? != 0 )); then
+  echo 'FAIL typescript lsp types' >&2  && exit 4
+fi
+
+echo "$result" \
+  | grep --quiet 'class Struct<Type = unknown, Schema = unknown>.*encapsulate the validation logic.*typescript'
+if (( $? != 0 )); then
+  echo 'FAIL typescript lsp'  && exit 3
+fi
+
+echo 'pass typescript lsp'
