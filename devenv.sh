d7b88313d87b27417792302e0668b70d4303e1f4
legobt <6wbvkn0j@anonaddy.me>
2024-06-30 11:58:54 +0000

    feat: add container-socker-proxy
    while it seems redundant with a separate http proxy when Caddy is there,
    keeping the socket proxy on an isolated network from other service
    ontainers doesn't hurt

--- devenv.sh
+++ devenv.sh
@@ -63,6 +63,10 @@ runtime_config () {
     COMPOSE_NETWORK_NAME="${COMPOSE_NETWORK_NAME:-internal}"
     NETWORK_NAME=${NETWORK_NAME:-$(get_compose_network_name "${COMPOSE_NETWORK_NAME}")}
   fi
+  if [[ -z "${CONTROL_NETWORK_NAME}" ]]; then
+    CONTROL_COMPOSE_NETWORK_NAME="${CONTROL_COMPOSE_NETWORK_NAME:-container-control}"
+    CONTROL_NETWORK_NAME=${CONTROL_NETWORK_NAME:-$(get_compose_network_name "${CONTROL_COMPOSE_NETWORK_NAME}")}
+  fi
 
   ### run args
   if [[ -n "${SSH_SOCKET}" ]]; then
@@ -286,6 +290,7 @@ ${cmd} run --rm -i \
   -e HOME=/home/user \
   -e "SRC_DIR=${SRC_DIR}" \
   --network "${NETWORK_NAME}" \
+  --network "${CONTROL_NETWORK_NAME}" \
   --dns "${CONTAINER_DNS}" \
   ${RUN_ARGS} \
   "${IMAGE}" \
