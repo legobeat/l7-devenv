b44cc66f655f51dfc31ff242f7b1fec21d0b5341
legobt <6wbvkn0j@anonaddy.me>
2024-07-03 09:57:19 +0000

    test: fix runner-node test; add test for pm shims

--- test/runner-node/test-corepack-pms.sh
+++ test/runner-node/test-corepack-pms.sh
@@ -7,6 +7,22 @@ for pm in yarn pnpm npm; do
   if [[ -n "${DEBUG}" ]]; then
     export RUN_ARGS="${RUN_ARGS} -e DEBUG=${DEBUG}"
   fi
-  find test/runner-node/fixtures/corepack -maxdepth 1 -name "${pm}*" \
-    -exec ./devenv.sh $(pwd)/test/runner-node/test-corepack-pm.sh {} ${pm} \;
+  for pmv in $(find test/runner-node/fixtures/corepack -maxdepth 1 -name "${pm}*") ; do
+    NAME=l7ide-test-runner-corepack ./devenv.sh $(pwd)/test/runner-node/test-corepack-pm.sh "${pmv}" "${pm}"
+  done
 done
+
+# test global shims
+for pm in yarn1 yarn3 yarn4 npm7 npm9 npm10 pnpm9; do
+  version=$(NAME="l7ide-test-runner-corepack" ./devenv.sh ${pm} --version)
+  # compare if major version is same as expected
+  if [[ "$(echo "${version}" | grep -o '[0-9]*' | head -n1)" != "$(echo "${pm}" | grep -o '[0-9]*' | head -n1)" ]]; then
+    echo "FAIL ${pm} is v${version}" >&2
+    export TESTFAIL=1
+    continue
+  fi
+  echo "PASS ${pm} is v${version}" >&2
+done
+if [[ -n "${TESTFAIL}" ]]; then
+  exit 1
+fi
