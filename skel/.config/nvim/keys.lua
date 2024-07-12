7dc82b0858d22b6bb4bcf491bf6d84e328274a62
legobt <6wbvkn0j@anonaddy.me>
2024-07-12 00:53:29 +0000

    nvim: toggleterm keybindings

--- skel/.config/nvim/keys.lua
+++ skel/.config/nvim/keys.lua
@@ -56,6 +56,21 @@ function _G.enable_lsp_keybindings(client, bufnr)
   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format() end, {desc = 'Format buffer', silent = true, buffer = bufnr })
 end
 
+-- convenient navigation out of ToggleTerm
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  --vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  --vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  -- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you want these for all terminals in nvim, use term://* instead
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+
 -- vim.keymap.set('n', 'lhs', 'rhs')
 -- same as vimscript
 -- nnoremap lhs rhs
